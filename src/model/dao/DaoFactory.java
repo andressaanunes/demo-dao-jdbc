d0a6ae1ece70a8236d6bea4f91f729e895fa4061
Andressa Nunes <andressa_assuncao@live.com>
2022-02-01 15:50:30 -0300

    findById implementation

--- src/model/dao/DaoFactory.java
+++ src/model/dao/DaoFactory.java
@@ -1,5 +1,18 @@
+
 package model.dao;
+
+import db.DB;
+import model.dao.impl.DepartmentDaoJDBC;
+import model.dao.impl.SellerDaoJDBC;
+import model.entities.SellerDao;
 
 public class DaoFactory {
 
+	public static SellerDao createSellerDao() {
+		return new SellerDaoJDBC(DB.getConnection());
+	}
+
+	public static DepartmentDao createDepartmentDao() {
+		return new DepartmentDaoJDBC(DB.getConnection());
+	}
 }
